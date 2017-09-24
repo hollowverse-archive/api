57d369da8cf194c17ac54c1b65ec31e8719fb681
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-24 20:36:25 +0300

    Revert "Remove body parser"
    This reverts commit aca071c121f814e5b50648aeaa4ad6030fc8d6f4.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { schema } from './schema';
 
 const app = express();
 
-app.use('/graphql', graphqlExpress({ schema }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
