8680552b21a3041141e1364419a4884e1ba506c2
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-24 21:27:17 +0300

    Reorder props

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,11 +4,11 @@ import { NotablePerson } from './entities/notablePerson';
 import { Event } from './entities/event';
 
 export const connection = createConnection({
+  type: 'mysql',
   database: 'hollowverse',
   host: 'localhost',
   username: 'root',
   password: '123456',
   port: 3306,
-  type: 'mysql',
   entities: [NotablePerson, Event],
 });
