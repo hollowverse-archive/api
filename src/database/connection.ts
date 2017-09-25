b9c9dd81866ef06666eb2bec59d8f234e164a93c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-25 22:20:10 +0300

    Support environment-specific db config

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,19 +1,40 @@
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
+import { createConnection, ConnectionOptions } from 'typeorm';
 import { NotablePerson } from './entities/notablePerson';
 import { Event } from './entities/event';
 import { Label } from './entities/label';
 import { Comment } from './entities/comment';
 import { User } from './entities/user';
 
-export const connection = createConnection({
+import * as fs from 'fs';
+import * as path from 'path';
+
+const DB_SECRET_FILE_PATH = path.resolve(
+  process.cwd(),
+  'secrets',
+  process.env.NODE_ENV === 'production' ? 'db.production.json' : 'db.json',
+);
+
+const { host, port, database, username, password }: DatabaseConfig = JSON.parse(
+  String(fs.readFileSync(DB_SECRET_FILE_PATH)),
+);
+
+const config: ConnectionOptions = {
   type: 'mysql',
-  database: 'hollowverse',
-  host: 'localhost',
-  username: 'root',
-  password: '123456',
+
+  // Do not any of these set to `true`,
+  // otherwise the database will be destroyed
+  synchronize: false,
   dropSchema: false,
-  synchronize: true,
-  port: 3306,
+
+  host,
+  port,
+  database,
+  username,
+  password,
+};
+
+export const connection = createConnection({
+  ...config,
   entities: [NotablePerson, Event, Label, Comment, User],
 });
---
+++ src/typings/db.production.json.d.ts
@@ -0,0 +1,7 @@
+declare type DatabaseConfig = {
+  host: string;
+  port: number;
+  database: string;
+  username: string;
+  password: string;
+};
