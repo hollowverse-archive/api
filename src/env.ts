6d704c155dcd43a0ce4fc1cf6d696133f318cefb
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-11-04 12:25:06 +0200

    Fix path to env.ts again

--- src/env.ts
+++ src/env.ts
@@ -1,8 +1,12 @@
 const {
+  NODE_ENV,
+
   // To use the production database, this must be set explicitly to 'true'
   // in EB environment settings. Otherwise, the testing database is used.
   USE_PRODUCTION_DATABASE,
 } = process.env;
 
+export const isProd = NODE_ENV === 'production';
+
 export const isUsingProductionDatabase =
-  process.env.NODE_ENV === 'production' && USE_PRODUCTION_DATABASE === 'true';
+  isProd && USE_PRODUCTION_DATABASE === 'true';
