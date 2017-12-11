068cfe5db11fe33125a2ee13b2ee31b07f2cbe39
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-11 21:47:24 +0200

    Add Algolia subscriber for NotablePerson entity

---
+++ src/database/subscribers/algolia.ts
@@ -0,0 +1,39 @@
+import * as algoliaSearch from 'algoliasearch';
+import {
+  EventSubscriber,
+  EntitySubscriberInterface,
+  InsertEvent,
+  RemoveEvent,
+  UpdateEvent,
+} from 'typeorm';
+
+import { NotablePerson } from '../entities/notablePerson';
+
+const applicationId = '';
+const apiKey = '';
+
+const algoliaClient = algoliaSearch(applicationId, apiKey);
+const index = algoliaClient.initIndex('notable-people');
+
+@EventSubscriber()
+export class AlgoliaSubscriber
+  implements EntitySubscriberInterface<NotablePerson> {
+  listenTo() {
+    return NotablePerson;
+  }
+
+  async afterInsert(event: InsertEvent<NotablePerson>) {
+    await index.addObject(event.entity, event.entity.id);
+  }
+
+  async afterRemove(event: RemoveEvent<NotablePerson>) {
+    await index.deleteObject(event.entityId);
+  }
+
+  async afterUpdate(event: UpdateEvent<NotablePerson>) {
+    await index.partialUpdateObject({
+      ...event.entity,
+      objectID: event.entity.id,
+    });
+  }
+}
