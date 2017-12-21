17c43c485920fb45797dd7fc499ded6ed592c9ad
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-21 13:35:10 +0200

    Add photo entity

---
+++ src/database/entities/Photo.ts
@@ -0,0 +1,49 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IsNotEmpty, ValidateIf, IsUrl } from 'class-validator';
+import { BaseEntity } from './BaseEntity';
+import { urlValidationOptions } from '../../helpers/validation';
+
+@Entity()
+export class Photo extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid') id: string;
+
+  @ValidateIf((_, v) => typeof v === 'string')
+  @IsNotEmpty()
+  @Column({ type: 'varchar', nullable: true })
+  description: string | null;
+
+  @Column({ type: 'datetime', nullable: false })
+  addedAt: Date;
+
+  @Column({ type: 'datetime', nullable: true })
+  takenAt: Date | null;
+
+  @ValidateIf((_, v) => typeof v === 'string')
+  @IsNotEmpty()
+  @Column({ type: 'varchar', nullable: true })
+  licence: string | null;
+
+  @ValidateIf((_, v) => typeof v === 'string')
+  @IsNotEmpty()
+  @Column({ type: 'varchar', nullable: true })
+  credits: string | null;
+
+  @Column({ type: 'tinyint', nullable: true })
+  isCopyrighted: boolean | null;
+
+  @Column({ type: 'tinyint', nullable: false })
+  isAttibutionRequired: boolean;
+
+  @IsUrl(urlValidationOptions)
+  @Column({ type: 'varchar', nullable: false })
+  sourceUrl: string;
+
+  @ValidateIf((_, v) => typeof v === 'string')
+  @IsNotEmpty()
+  @Column({ type: 'varchar', nullable: true })
+  artist: string | null;
+
+  @IsNotEmpty()
+  @Column({ type: 'varchar', nullable: false })
+  s3Path: string;
+}
