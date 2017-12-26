42a032eb6c29198da08f5a3c8fdfa5ac4b462c3a
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-12-26 16:28:03 +0200

    Add query to browse notable people
    The query defines types compliant with the [Relay Spec](https://facebook.github.io/relay/graphql/connections.htm) to make it easier achieve full compatibility to Relay in the future.

---
+++ src/resolvers/queries/notablePeople.ts
@@ -0,0 +1,35 @@
+import { FindManyOptions } from 'typeorm';
+import { connection } from '../../database/connection';
+import { NotablePerson } from '../../database/entities/NotablePerson';
+import { ResolverMap } from '../../typings/resolverMap';
+
+export const resolvers: Partial<ResolverMap> = {
+  RootQuery: {
+    async notablePeople(_, { after, first }) {
+      const skip = (Number(after) || 0) + 1;
+      const db = await connection;
+      const query: FindManyOptions<NotablePerson> = {
+        order: {
+          name: 'ASC',
+        },
+      };
+      const notablePeople = db.getRepository(NotablePerson);
+
+      const [people, all] = await notablePeople.findAndCount({
+        ...query,
+        take: first,
+        skip,
+      });
+
+      return {
+        edges: people.map((person, i) => ({
+          node: person,
+          cursor: i + skip,
+        })),
+        pageInfo: {
+          hasNextPage: skip + people.length < all,
+        },
+      };
+    },
+  },
+};
