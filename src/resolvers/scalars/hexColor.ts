2d7300bda8f3f720e9779c6f77e2b35e81b95e81
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-01-31 20:26:04 +0200

    Add resolvers for HexColor

---
+++ src/resolvers/scalars/hexColor.ts
@@ -0,0 +1,31 @@
+import { GraphQLScalarType } from 'graphql/type';
+import { ASTNode, Kind } from 'graphql/language';
+import { GraphQLError } from 'graphql/error';
+
+import * as isHexColor from 'validator/lib/isHexColor';
+
+// Called when the user passes the value inline
+const parseLiteral = (ast: ASTNode) => {
+  if (ast.kind === Kind.STRING && isHexColor(ast.value)) {
+    return ast.value;
+  }
+
+  throw new GraphQLError('Invalid value for HexColor', [ast]);
+};
+
+export const HexColor = new GraphQLScalarType({
+  name: 'HexColor',
+  description:
+    'A valid hexadecimal color string.',
+
+  parseLiteral,
+
+  // Called when the user passes the value as an input variable
+  parseValue(value) {
+    return parseLiteral({ kind: Kind.STRING, value });
+  },
+
+  serialize(value) {
+    return String(value);
+  },
+});
