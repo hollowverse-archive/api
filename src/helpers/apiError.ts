4527c2327030f126332d78d489cfe114e858083c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-02-05 22:06:31 +0200

    (breaking) Treat user errors as data

--- src/helpers/apiError.ts
+++ src/helpers/apiError.ts
@@ -1,37 +1,37 @@
-const messagesByErrorType = {
-  OperationNotAllowedError: 'This operation is not allowed',
-  MustBeAuthorizedError:
+import { UserErrorType } from '../typings/schema';
+
+const messagesByErrorType: Record<UserErrorType, string> = {
+  OPERATION_NOT_ALLOWED: 'This operation is not allowed',
+  AUTHENTICATION_REQUIRED:
     'This operation requires that the request is authenticated',
-  InvalidAccessTokenError:
+  INVALID_ACCESS_TOKEN:
     'The passed access token is either empty, expired or invalid',
 };
 
-type ApiErrorType = keyof typeof messagesByErrorType;
-
-type ApiErrorOptions = {
+type UserErrorOptions = {
   isSensitive: boolean;
 };
 
-const defaultOptions: ApiErrorOptions = {
+const defaultOptions: UserErrorOptions = {
   isSensitive: true,
 };
 
 /**
- * Custom API error class, requires a pre-defined
+ * Custom error class, requires a pre-defined
  * error type and handles removing potentially sensitive properties.
  */
-export class ApiError extends Error {
+export class UserError extends Error {
   /**
-   * @param type One of the pre-defined API error types.
+   * @param type One of the pre-defined user error types.
    * @param message If not defined, the default error message for the specified
    * error type is used.
    * @param options When `options.isSensitive` is set to `true`,
    * it makes sure to remove sensitive props from the error object.
    */
   constructor(
-    type: ApiErrorType,
+    type: UserErrorType,
     message: string = messagesByErrorType[type],
-    options: ApiErrorOptions = defaultOptions,
+    options: UserErrorOptions = defaultOptions,
   ) {
     super();
     this.name = type;
