86b7845a0015bbaba80fe1a61a14b773440cefdb
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-07 11:40:13 +0200

    Add initial directiveResolvers

--- src/schema.ts
+++ src/schema.ts
@@ -2,10 +2,24 @@ import { makeExecutableSchema } from 'graphql-tools';
 import { readFile } from './helpers/readFile';
 
 import { resolvers } from './resolvers/resolvers';
+import { ApiError } from './helpers/apiError';
 
 export const schema = readFile('schema.graphql', 'utf8').then(typeDefs =>
   makeExecutableSchema({
     typeDefs,
     resolvers: resolvers as any,
+    directiveResolvers: {
+      requireAuth(next, _source, _args, context) {
+        if (context.viewer) {
+          return next();
+        } else {
+          throw new ApiError('MustBeAuthorizedError');
+        }
+      },
+      requireRoles(_next, _source, _args, _context) {
+        // @TODO: implement this
+        throw new ApiError('MustBeAuthorizedError');
+      },
+    },
   }),
 );
