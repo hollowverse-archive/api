36ea6ceae486b1f5471b648ccd16f8b1c4843822
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-03-25 19:48:09 +0200

    Switch to @hollowverse/config and @hollowverse/utils

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,2 @@
-module.exports = require('@hollowverse/common/prettier.config');
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+module.exports = require('@hollowverse/config/prettier.config');
