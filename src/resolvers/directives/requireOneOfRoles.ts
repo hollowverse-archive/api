3fbcac25f9b7ba557ae1e156037cdf443a0b3b61
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-05 22:03:56 +0300

    Fix resolver name

--- src/resolvers/directives/requireOneOfRoles.ts
+++ src/resolvers/directives/requireOneOfRoles.ts
@@ -1,7 +1,7 @@
 import { DirectiveResolverMap } from '../../typings/resolverMap';
 
-export const resolvers: Pick<DirectiveResolverMap, 'requireRoles'> = {
-  async requireRoles(_next, _source, _args, _context) {
+export const resolvers: Pick<DirectiveResolverMap, 'requireOneOfRoles'> = {
+  async requireOneOfRoles(_next, _source, _args, _context) {
     throw new Error('Roles for users are not implemented yet');
   },
 };
