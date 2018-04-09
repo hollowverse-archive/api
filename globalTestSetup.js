1dd8f2ac642d72e0f1f324ea9cef2484438b581d
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-09 23:44:19 +0300

    Fix Promise.race in globalTestSetup.js
    The way the code was written meant that the waiting message would always get printed.

--- globalTestSetup.js
+++ globalTestSetup.js
@@ -8,40 +8,49 @@ const bluebird = require('bluebird');
 module.exports = async () => {
   const DB_PORT = process.env.TEST_DB_PORT || 3306;
   const DB_HOST = process.env.TEST_DB_HOST || 'localhost';
-  const waitForConnection = waitForMySql.await({
-    port: Number(DB_PORT),
-    host: DB_HOST,
-    user: 'root',
-    password: '123456',
-    query: 'SHOW DATABASES',
-    quiet: true,
+  let hasConnected = false;
+
+  const waitForConnection = waitForMySql
+    .await({
+      port: Number(DB_PORT),
+      host: DB_HOST,
+      user: 'root',
+      password: '123456',
+      query: 'SHOW DATABASES',
+      quiet: true,
+    })
+    .then(() => {
+      hasConnected = true;
+    });
+
+  const delay = bluebird.delay(1500).then(() => {
+    if (hasConnected) {
+      return;
+    }
+
+    console.info('\n');
+    console.info(stripIndent`
+        Waiting for MySQL server on port ${DB_PORT}...
+        This server will be used to create temporary databases for running the
+        tests.
+  
+        If you do not have a local MySQL server, you can spin up a Docker container
+        with the following command in another shell:
+  
+          docker run -it \\
+           -p ${DB_PORT}:3306 \\
+           --name hollowverse-test-db-server \\
+           -e MYSQL_ROOT_PASSWORD=123456 \\
+          --tmpfs=/var/lib/mysql/:rw,noexec,nosuid,size=2000m \\
+          --tmpfs=/tmp/:rw,noexec,nosuid,size=50m \\
+           mysql:5.6
+        
+        This will launch a MySQL server with the database data stored in memory.
+      `);
   });
 
   if (!process.env.CI) {
-    await Promise.race([
-      bluebird.delay(2000).then(() => {
-        console.info('\n');
-        console.info(stripIndent`
-            Waiting for MySQL server on port ${DB_PORT}...
-            This server will be used to create temporary databases for running the
-            tests.
-      
-            If you do not have a local MySQL server, you can spin up a Docker container
-            with the following command in another shell:
-      
-              docker run -it \\
-               -p ${DB_PORT}:3306 \\
-               --name hollowverse-test-db-server \\
-               -e MYSQL_ROOT_PASSWORD=123456 \\
-              --tmpfs=/var/lib/mysql/:rw,noexec,nosuid,size=2000m \\
-              --tmpfs=/tmp/:rw,noexec,nosuid,size=50m \\
-               mysql:5.6
-            
-            This will launch a MySQL server with the database data stored in memory.
-          `);
-      }),
-      waitForConnection,
-    ]);
+    await Promise.race([delay, waitForConnection]);
   }
 
   return waitForConnection;
