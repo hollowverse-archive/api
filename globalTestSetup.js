550303bbcd601154ac0544874e30836b2b2cca48
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-14 18:46:22 +0300

    Serve playground on /, improve test instructions and use a different port for tests (#82)
    Part of our efforts to improve the development experience :smile:

--- globalTestSetup.js
+++ globalTestSetup.js
@@ -6,7 +6,7 @@ const waitForMySql = require('wait-for-mysql');
 const bluebird = require('bluebird');
 
 module.exports = async () => {
-  const DB_PORT = process.env.TEST_DB_PORT || 3306;
+  const DB_PORT = process.env.TEST_DB_PORT || 3307;
   const DB_HOST = process.env.TEST_DB_HOST || 'localhost';
   let hasConnected = false;
 
@@ -21,6 +21,8 @@ module.exports = async () => {
     })
     .then(() => {
       hasConnected = true;
+      process.env.TEST_DB_PORT = String(DB_PORT);
+      process.env.TEST_DB_HOST = DB_HOST;
     });
 
   const promises = [waitForConnection];
@@ -28,24 +30,32 @@ module.exports = async () => {
   if (!process.env.CI) {
     const delay = bluebird.delay(1500).then(() => {
       if (!hasConnected) {
+        process.env.DELAY_EXCEEDED = 'true';
+
         console.info('\n');
         console.info(stripIndent`
-        Waiting for MySQL server on port ${DB_PORT}...
-        This server will be used to create temporary databases for running the
-        tests.
-  
-        If you do not have a local MySQL server, you can spin up a Docker container
-        with the following command in another shell:
-  
-          docker run -it \\
-           -p ${DB_PORT}:3306 \\
-           --name hollowverse-test-db-server \\
-           -e MYSQL_ROOT_PASSWORD=123456 \\
-          --tmpfs=/var/lib/mysql/:rw,noexec,nosuid,size=2000m \\
-          --tmpfs=/tmp/:rw,noexec,nosuid,size=50m \\
-           mysql:5.6
-        
-        This will launch a MySQL server with the database data stored in memory.
+          Waiting for MySQL server on port ${DB_PORT}...
+          This server will be used to create temporary databases for running the
+          tests.
+
+          If you do not have a local MySQL server, you can spin up a Docker container
+          with the following command in another shell:
+
+            docker run -it \\
+              -d \\
+              -p ${DB_PORT}:3306 \\
+              --name hollowverse-test-db-server \\
+              -e MYSQL_ROOT_PASSWORD=123456 \\
+              --tmpfs=/var/lib/mysql/:rw,noexec,nosuid,size=2000m \\
+              --tmpfs=/tmp/:rw,noexec,nosuid,size=50m \\
+            mysql:5.6
+
+          This will launch a MySQL server with the database data stored in memory.
+
+          If you have run the above command before, the container might be stopped,
+          you can start it again:
+
+            docker start hollowverse-test-db-server
       `);
       }
 
