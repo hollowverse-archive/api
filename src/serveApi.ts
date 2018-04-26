edfb10f5ace3a28f991d4462a558f30631c25ba4
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-04-26 22:51:13 +0300

    Add boilerplate

---
+++ src/serveApi.ts
@@ -0,0 +1,17 @@
+import awsServerlessExpress from 'aws-serverless-express';
+import { createApiServer } from './server';
+
+const serverPromise = createApiServer().then(expressApp =>
+  awsServerlessExpress.createServer(expressApp as any, undefined, ['*/*']),
+);
+
+export const serveApi: AWSLambda.Handler = async (event, context) =>
+  serverPromise.then(async server => {
+    // tslint:disable-next-line promise-must-complete
+    return new Promise(resolve => {
+      // See https://github.com/awslabs/aws-serverless-express/issues/134
+      context.succeed = resolve;
+
+      awsServerlessExpress.proxy(server, event, context);
+    });
+  });
