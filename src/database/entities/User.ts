5ee017109e57e2e15cce520c26760a8afb38a410
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2018-08-29 20:36:57 +0300

    Fix boolean type

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -59,7 +59,7 @@ export class User extends BaseEntity {
   @Column({ type: 'datetime', nullable: false })
   signedUpAt: Date;
 
-  @Column({ type: 'bool', nullable: false, default: false })
+  @Column({ type: 'tinyint', nullable: false, default: false })
   isBanned: boolean;
 
   @IsNotEmpty()
